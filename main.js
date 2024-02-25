19ab9cb84e07a2d72d2b6ffb7addc5df7fa233c3
Divyang Dhameliya <dhameliyadivyang@gmail.com>
2024-02-25 10:21:31 +0530

    Created main2.js

---
+++ main.js
@@ -0,0 +1,38 @@
+var n=4;
+function print_mat(){
+    var tbl = document.getElementById('tbl');
+        tbl.innerHTML='';
+    n = document.getElementById('in').value;
+    console.log(n);
+    tbl.id = "tbl";
+    for(let i=0;i<n;i++){
+        var row = document.createElement('tr');
+        for(let j=0;j<n;j++){
+        var cell = document.createElement('td');
+        var in_field = document.createElement('input');
+        in_field.type = "text";
+        cell.appendChild(in_field);
+        row.appendChild(cell);
+        }
+        tbl.appendChild(row);
+    }
+}
+pjmatrix = [];
+
+function insert_data(){
+    const tem = tbl.querySelectorAll('input');   
+    pjmatrix = []; // pj matrix shows person job data
+    for(var i=0;i<n;i++){
+        var r = [];
+        for(var j=0;j<n;j++){
+            if(tem[n*i + j].value == ''){ // if any cell is empty 
+                alert("fill value");
+                return;
+            }
+            r.push(tem[n*i + j].value);
+        }
+        pjmatrix.push(r);
+    }
+    console.log(pjmatrix);
+    window.location.href = `home2.html?pjmatrix=${encodeURIComponent(pjmatrix)}`;
+}
